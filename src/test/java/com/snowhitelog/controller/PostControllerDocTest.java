65aef8575d45ef9dec878298236f68c12339fdf2
kim-sun-a <gina305496@gmail.com>
2023-03-25 15:27:16 +0900

    REST API DOCS 커스터마이징

--- src/test/java/com/snowhitelog/controller/PostControllerDocTest.java
+++ src/test/java/com/snowhitelog/controller/PostControllerDocTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snowhitelog.domain.Post;
 import com.snowhitelog.repository.PostRepository;
 import com.snowhitelog.request.PostCreate;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,24 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.payload.PayloadDocumentation;
-import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,7 +53,7 @@ public class PostControllerDocTest {
         this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", 1L).accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index", pathParameters(
+                .andDo(document("post-inquiry", pathParameters(
                                 parameterWithName("postId").description("게시글 ID")
                         ),
                         responseFields(
@@ -91,9 +84,9 @@ public class PostControllerDocTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index", requestFields(
-                        fieldWithPath("title").description("제목"),
-                        fieldWithPath("content").description("내용")
+                .andDo(document("post-create", requestFields(
+                        fieldWithPath("title").description("제목").attributes(key("constraint").value("좋은 제목 입력해주세요")),
+                        fieldWithPath("content").description("내용").optional()
                         )
                 ));
     }
