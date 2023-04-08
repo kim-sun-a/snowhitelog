92994183eaf5564c0a64409e9ae37a6b41771484
kim-sun-a <gina305496@gmail.com>
2023-04-08 17:20:04 +0900

    글 수정하기 vue update

--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {defineProps, onMounted, ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const props = defineProps({
   postId: {
@@ -13,7 +14,13 @@ const post = ref({
   id: 0,
   title : "",
   content: ""
-})
+});
+
+const router = useRouter();
+
+const moveToEdit = () => {
+  router.push({name: "edit", params:{postId: props.postId}})
+}
 
 onMounted(() => {
   axios.get(`/api/posts/${props.postId}`).then(response => {
@@ -26,4 +33,6 @@ onMounted(() => {
   <div>
     {{ post.content }}
   </div>
+
+  <el-button type="warning" @click="moveToEdit()">수정</el-button>
 </template>
