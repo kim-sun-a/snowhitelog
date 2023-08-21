ec34c353267444d65c89adc7b5b1eae25571b163
sakim <gina305496@gmail.com>
2023-08-21 14:53:30 +0900

    세션 쿠키 검증 추가

--- src/main/java/com/snowhitelog/config/AuthResolver.java
+++ src/main/java/com/snowhitelog/config/AuthResolver.java
@@ -5,14 +5,18 @@ import com.snowhitelog.domain.Session;
 import com.snowhitelog.exception.Unauthorized;
 import com.snowhitelog.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -25,11 +29,18 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null  || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if(servletRequest == null ) {
+            log.error("servletRequest null");
+            throw new Unauthorized();
+        }
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies.length == 0) {
+            log.error("쿠키 없음");
             throw new Unauthorized();
         }
 
+        String accessToken = cookies[0].getValue();
         Session session = sessionRepository.findByAccessToken(accessToken).orElseThrow(Unauthorized::new);
 
         return new UserSection(session.getUser().getId());
--- src/main/java/com/snowhitelog/controller/AuthController.java
+++ src/main/java/com/snowhitelog/controller/AuthController.java
@@ -9,10 +9,14 @@ import com.snowhitelog.response.SessionResponse;
 import com.snowhitelog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.Optional;
 
 @Slf4j
@@ -23,11 +27,22 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
+    public ResponseEntity<Object> login(@RequestBody Login login) {
         // json id/pw
         log.info(">>>login={}", login);
         // db 조회
         String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost")        // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(60))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>> cookie ={}", cookie.toString());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 }
